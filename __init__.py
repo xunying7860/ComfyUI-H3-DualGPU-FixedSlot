b65ae86e26134ac391b2303738f8a51711d26b3d
xunying7860 <3991015908@qq.com>
2026-08-16 22:34:48 +0800

    上传 __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""ComfyUI-H3-DualGPU-FixedSlot：MiniMax H3 双卡固定槽流水节点包。"""
+
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
